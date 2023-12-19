a531409fc0b7f44eff22092b4111f880709a87e1
drotarovSprievodca <labajpeter98@gmail.com>
2023-12-19 11:42:45 +0100

    dpld from bdd

--- GenerovanieBDD/GenerovanieBDD.cpp
+++ GenerovanieBDD/GenerovanieBDD.cpp
@@ -5,12 +5,15 @@
 #include <vector>
 #include <array>
 #include <libteddy/details/pla_file.hpp>
+#include "libteddy/details/reliability_manager.hpp"
+#include <libteddy/reliability.hpp>
 
 int main()
 {
     std::optional<teddy::pla_file> pla_file = teddy::pla_file::load_file("C:\\Users\\DELL\\git\\Diplomka\\GenerovanieBDD\\PLA\\5xp1.pla");
 
     if (pla_file.has_value()) {
+
         teddy::pla_file* pla = &pla_file.value();
 
         std::cout << "Number of function in file: " << pla->get_function_count() << std::endl;
@@ -44,13 +47,30 @@ int main()
             std::cout << "\n";
         }
         std::cout << "" << std::endl;
+        
 
-        teddy::bdd_manager manager(pla->get_variable_count(), 1'000);
+        teddy::bss_manager manager(pla->get_variable_count(), 1'000);
+        teddy::bss_manager::diagram_t diagram = manager.from_pla(*pla, teddy::fold_type::Tree)[0];
+        // premenna x0 klesne z 1 -> 0 a funkcia klesne z 1 -> 0
+        teddy::bss_manager::diagram_t dpbd = manager.dpld({ 0, 1, 0 }, teddy::dpld::type_1_decrease(1), diagram);
+        manager.to_dot_graph(std::cout, diagram);
+        std::ofstream ofst("f.dot");
+        manager.to_dot_graph(ofst, diagram);
+
+
+
+
+
+
+
+
+        /*
         auto vector_of_diagrams = manager.from_pla(*pla, teddy::fold_type::Tree);
-        for (auto diagram : vector_of_diagrams) {
+        for (teddy::diagram<void, teddy::degrees::fixed<2>> diagram : vector_of_diagrams) {
             manager.to_dot_graph(std::cout, diagram);
             std::ofstream ofst("f.dot");
             manager.to_dot_graph(ofst, diagram);
         }
+        */
     }
 }
