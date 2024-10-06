7df3fa905f3095809d6fc34b9674ac92a5b99089
drotarovSprievodca <labajpeter98@gmail.com>
2024-10-06 21:54:20 +0200

    ordering for all pla based on td

--- GenerovanieBDD/GenerovanieBDD.cpp
+++ GenerovanieBDD/GenerovanieBDD.cpp
@@ -16,79 +16,181 @@ struct var {
     double true_density;
 };
 
-bool compare_by_true_density(const var& a, const var& b) {
+bool compare_by_true_density_desc(const var& a, const var& b) {
     return a.true_density > b.true_density;
 }
 
-int main()
-{
-    std::string comparing_options[] = {
-        "ORIGINAL",
-        "ASCENDING_TD",
-        "DESCENDING_TD",
-        "RANDOM"
-    };
+bool compare_by_true_density_asc(const var& a, const var& b) {
+    return a.true_density < b.true_density;
+}
 
-    for (std::string comparing_option : comparing_options) {
-        std::cout << comparing_option << std::endl;
-        std::string directory_path = "C:\\Users\\DELL\\git\\Diplomka\\GenerovanieBDD\\ALL_PLA\\";
-        WIN32_FIND_DATA find_file_data;
-        HANDLE h_find = FindFirstFile((directory_path + "*").c_str(), &find_file_data);
+void get_td_of_all_vars_in_function(teddy::bss_manager& manager, std::vector<var>& list_for_reordering, teddy::bss_manager::diagram_t& diagram) {
+    for (int i = 0; i < list_for_reordering.size(); ++i) {
+        // variable xi decreases from 1 -> 0 while function decreases from 1 -> 0
+        teddy::bss_manager::diagram_t dpbd_decrease = manager.dpld({ i, 1, 0 }, teddy::dpld::type_1_decrease(1), diagram);
 
-        if (h_find == INVALID_HANDLE_VALUE) {
-            std::cerr << "Could not open directory!" << std::endl;
-            return 1;
-        }
-        
-        int sum_of_node_counts = 0;
-        int number_of_diagrams = 0;
+        // variable xi increases from 0 -> 1 while function decreases from 1 -> 0
+        teddy::bss_manager::diagram_t dpbd_increase = manager.dpld({ i, 0, 1 }, teddy::dpld::type_1_decrease(1), diagram);
 
-        do {
-            if (std::string(find_file_data.cFileName) == "." || std::string(find_file_data.cFileName) == "..") { 
-                continue; 
-            }
-            std::string pla_path = directory_path + find_file_data.cFileName;
 
-            std::optional<teddy::pla_file> pla_file = teddy::pla_file::load_file(pla_path);
-            if (!pla_file.has_value()) { 
-                continue; 
-            }
+        // number of 1 in dpbd_decrease
+        int number_of_ones_decrease = manager.satisfy_all<std::vector<int>>(1, dpbd_decrease).size() / 2;
 
-            teddy::pla_file* pla = &pla_file.value();
-            int number_of_vars = pla->get_variable_count();
-            int number_of_functions = pla->get_function_count();
+        // number of 0 in dpbd_decrease
+        int number_of_zeros_decrease = manager.satisfy_all<std::vector<int>>(0, dpbd_decrease).size() / 2;
 
-            if (comparing_option == "ORIGINAL") {
-                // creating manager with default order of variables
-                teddy::bss_manager manager(number_of_vars, 33'000);
-                manager.set_auto_reorder(false); // Disable automatic variable reordering.
+        // number of 1 in dpbd_increase
+        int number_of_ones_increase = manager.satisfy_all<std::vector<int>>(1, dpbd_increase).size() / 2;
 
-                std::cout << "There is " << std::to_string(number_of_functions) << " functions in file: " + std::string(find_file_data.cFileName) << std::endl;
-                for (int i = 0; i < number_of_functions; ++i) {
-                    teddy::bss_manager::diagram_t diagram = manager.from_pla(*pla, teddy::fold_type::Tree)[i];
-                    int node_count = manager.get_node_count(diagram);
-                    std::cout << "Number of nodes in diagram (including terminal nodes): " << node_count << std::endl;
-                    sum_of_node_counts += node_count;
-                    number_of_diagrams += 1;
-                }
-                std::cout << '\n' << std::endl;
+        // number of 0 in dpbd_increase
+        int number_of_zeros_increase = manager.satisfy_all<std::vector<int>>(0, dpbd_increase).size() / 2;
 
-            } else if (comparing_option == "ASCENDING_TD") {
-                // pass
-            } else if (comparing_option == "DESCENDING_TD") {
-                // pass
-            } else if (comparing_option == "RANDOM") {
-                // pass
-            }
 
-        } while (FindNextFile(h_find, &find_file_data) != 0);
-        FindClose(h_find);
-        if (number_of_diagrams != 0) {
-            double average_number_of_nodes = sum_of_node_counts / number_of_diagrams;
-            std::cout << "Average number of nodes with " + comparing_option + " order is: " + std::to_string(average_number_of_nodes) << std::endl;
-        }  
+        double td_for_decrease = (double)number_of_ones_decrease / (number_of_ones_decrease + number_of_zeros_decrease);
+        double td_for_increase = (double)number_of_ones_increase / (number_of_ones_increase + number_of_zeros_increase);
+
+        double true_density = td_for_decrease + td_for_increase;
+
+        var td_var;
+        td_var.true_density = true_density;
+        td_var.variable = i;
+
+        list_for_reordering[i] = td_var;
     }
 }
+
+int main() {
+
+    std::string comparing_option = "ASCENDING_TD"; // ASCENDING_TD, DESCENDING_TD, RANDOM
+    std::cout << comparing_option << std::endl;
+
+    std::string directory_path = "C:\\Users\\DELL\\git\\Diplomka\\GenerovanieBDD\\ALL_PLA\\";
+    WIN32_FIND_DATA find_file_data;
+    HANDLE h_find = FindFirstFile((directory_path + "*").c_str(), &find_file_data);
+
+    if (h_find == INVALID_HANDLE_VALUE) {
+        std::cerr << "Could not open directory!" << std::endl;
+        return 1;
+    }
+        
+    int sum_of_node_counts = 0;
+    int number_of_diagrams = 0;
+
+    do {
+        if (std::string(find_file_data.cFileName) == "." || std::string(find_file_data.cFileName) == "..") { 
+            continue; 
+        }
+        std::string pla_path = directory_path + find_file_data.cFileName;
+
+        std::optional<teddy::pla_file> pla_file = teddy::pla_file::load_file(pla_path);
+        if (!pla_file.has_value()) { 
+            continue; 
+        }
+
+        teddy::pla_file* pla = &pla_file.value();
+
+        // number of variables in this pla file
+        int number_of_vars = pla->get_variable_count();
+
+        // number of functions in this pla file
+        int number_of_functions = pla->get_function_count();
+
+        // Creating manager with default order of variables
+        teddy::bss_manager manager(number_of_vars, 33'000);
+
+        // Disable automatic variable reordering.
+        manager.set_auto_reorder(false); 
+
+        std::cout << "There is " << std::to_string(number_of_functions) << " functions with " << std::to_string(number_of_vars) << " variables in file: " + std::string(find_file_data.cFileName) << std::endl;
+        if (comparing_option == "ORIGINAL") {
+            for (int i = 0; i < number_of_functions; ++i) {
+                // get function from pla file
+                teddy::bss_manager::diagram_t diagram = manager.from_pla(*pla, teddy::fold_type::Tree)[i];
+                
+                // save statistics
+                int node_count = manager.get_node_count(diagram);
+                //std::cout << "Number of nodes in diagram (including terminal nodes): " << node_count << std::endl;
+                sum_of_node_counts += node_count;
+                number_of_diagrams += 1;
+            }
+        } else if (comparing_option == "DESCENDING_TD") {
+            for (int i = 0; i < number_of_functions; ++i) {
+                // get function from pla file
+                teddy::bss_manager::diagram_t diagram = manager.from_pla(*pla, teddy::fold_type::Tree)[i];
+
+                // list for true densities of all variables of this function
+                std::vector<var> list_for_reordering = std::vector<var>(number_of_vars);
+                
+                // save all true densities of each variables in this function in list_for_reordering
+                get_td_of_all_vars_in_function(manager, list_for_reordering, diagram);
+                
+                // sort list of structs based on true density
+                std::sort(list_for_reordering.begin(), list_for_reordering.end(), compare_by_true_density_desc);
+                
+                // vector with new order in teddy format 
+                std::vector<teddy::int32> order_after = std::vector<teddy::int32>(number_of_vars);
+
+                // moving reordered list to teddy format vector
+                for (int i = 0; i < number_of_vars; ++i) {
+                    order_after[i] = list_for_reordering[i].variable;
+                }
+
+                // creating manager with new order of variables based on true density
+                teddy::bss_manager manager_after(number_of_vars, 1'000, order_after);
+                manager_after.set_auto_reorder(false);
+                teddy::bss_manager::diagram_t diagram_after = manager_after.from_pla(*pla, teddy::fold_type::Tree)[0];
+
+                // save statistics
+                int node_count = manager_after.get_node_count(diagram_after);
+                sum_of_node_counts += node_count;
+                number_of_diagrams += 1;
+            }
+        } else if (comparing_option == "ASCENDING_TD") {
+            for (int i = 0; i < number_of_functions; ++i) {
+                // get function from pla file
+                teddy::bss_manager::diagram_t diagram = manager.from_pla(*pla, teddy::fold_type::Tree)[i];
+
+                // list for true densities of all variables of this function
+                std::vector<var> list_for_reordering = std::vector<var>(number_of_vars);
+
+                // save all true densities of each variables in this function in list_for_reordering
+                get_td_of_all_vars_in_function(manager, list_for_reordering, diagram);
+
+                // sort list of structs based on true density
+                std::sort(list_for_reordering.begin(), list_for_reordering.end(), compare_by_true_density_asc);
+
+                // vector with new order in teddy format 
+                std::vector<teddy::int32> order_after = std::vector<teddy::int32>(number_of_vars);
+
+                // moving reordered list to teddy format vector
+                for (int i = 0; i < number_of_vars; ++i) {
+                    order_after[i] = list_for_reordering[i].variable;
+                }
+
+                // creating manager with new order of variables based on true density
+                teddy::bss_manager manager_after(number_of_vars, 1'000, order_after);
+                manager_after.set_auto_reorder(false);
+                teddy::bss_manager::diagram_t diagram_after = manager_after.from_pla(*pla, teddy::fold_type::Tree)[0];
+
+                // save statistics
+                int node_count = manager_after.get_node_count(diagram_after);
+                sum_of_node_counts += node_count;
+                number_of_diagrams += 1;
+                manager_after.clear_cache();
+            } 
+        } else if (comparing_option == "RANDOM") {
+            // pass
+        }
+    } while (FindNextFile(h_find, &find_file_data) != 0);
+    FindClose(h_find);
+
+    // print statistics
+    if (number_of_diagrams != 0) {
+        double average_number_of_nodes = sum_of_node_counts / number_of_diagrams;
+        std::cout << "Average number of nodes with " + comparing_option + " order is: " + std::to_string(average_number_of_nodes) << std::endl;
+    }  
+}
+
     
 
 
