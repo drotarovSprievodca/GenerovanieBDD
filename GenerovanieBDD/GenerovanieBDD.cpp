c7dfd6a1e095b11966821186249c5234c6a591fe
drotarovSprievodca <labajpeter98@gmail.com>
2024-10-15 21:29:54 +0200

    Added random order

--- GenerovanieBDD/GenerovanieBDD.cpp
+++ GenerovanieBDD/GenerovanieBDD.cpp
@@ -10,6 +10,7 @@
 #include <libteddy/reliability.hpp>
 #include <algorithm>
 #include <windows.h>
+#include <random>
 
 struct var {
     int variable;
@@ -59,12 +60,57 @@ void get_td_of_all_vars_in_function(teddy::bss_manager& manager, std::vector<var
     }
 }
 
+void use_derivatives(double &sum_of_node_counts, 
+                     double &number_of_diagrams, 
+                     int number_of_functions, 
+                     int number_of_vars, 
+                     teddy::bss_manager &manager,
+                     teddy::pla_file* pla,
+                     bool ascending) {
+
+    for (int i = 0; i < number_of_functions; ++i) {
+        // get function from pla file
+        teddy::bss_manager::diagram_t diagram = manager.from_pla(*pla, teddy::fold_type::Tree)[i];
+
+        // list for true densities of all variables of this function
+        std::vector<var> list_for_reordering = std::vector<var>(number_of_vars);
+
+        // save all true densities of each variables in this function in list_for_reordering
+        get_td_of_all_vars_in_function(manager, list_for_reordering, diagram);
+
+        // sort list of structs based on true density
+        if (ascending) {
+            std::sort(list_for_reordering.begin(), list_for_reordering.end(), compare_by_true_density_asc);
+        } else {
+            std::sort(list_for_reordering.begin(), list_for_reordering.end(), compare_by_true_density_desc);
+        }
+        
+        // vector with new order in teddy format 
+        std::vector<teddy::int32> order_after = std::vector<teddy::int32>(number_of_vars);
+
+        // moving reordered list to teddy format vector
+        for (int i = 0; i < number_of_vars; ++i) {
+            order_after[i] = list_for_reordering[i].variable;
+        }
+
+        // creating manager with new order of variables based on true density
+        teddy::bss_manager manager_after(number_of_vars, 1'000, order_after);
+        manager_after.set_auto_reorder(false);
+        teddy::bss_manager::diagram_t diagram_after = manager_after.from_pla(*pla, teddy::fold_type::Tree)[0];
+
+        // save statistics
+        int node_count = manager_after.get_node_count(diagram_after);
+        sum_of_node_counts += node_count;
+        number_of_diagrams += 1;
+    }
+}
+
 int main() {
 
-    std::string comparing_option = "ASCENDING_TD"; // ASCENDING_TD, DESCENDING_TD, RANDOM
+    std::string comparing_option = "RANDOM"; // ORIGINAL, ASCENDING_TD, DESCENDING_TD, RANDOM
     std::cout << comparing_option << std::endl;
 
-    std::string directory_path = "C:\\Users\\DELL\\git\\Diplomka\\GenerovanieBDD\\ALL_PLA\\";
+    std::string directory_path = "C:\\Users\\DELL\\git\\Diplomka\\GenerovanieBDD\\SOME_PLA\\";
     WIN32_FIND_DATA find_file_data;
     HANDLE h_find = FindFirstFile((directory_path + "*").c_str(), &find_file_data);
 
@@ -73,18 +119,18 @@ int main() {
         return 1;
     }
         
-    int sum_of_node_counts = 0;
-    int number_of_diagrams = 0;
+    double sum_of_node_counts = 0;
+    double number_of_diagrams = 0;
 
     do {
-        if (std::string(find_file_data.cFileName) == "." || std::string(find_file_data.cFileName) == "..") { 
-            continue; 
+        if (std::string(find_file_data.cFileName) == "." || std::string(find_file_data.cFileName) == "..") {
+            continue;
         }
         std::string pla_path = directory_path + find_file_data.cFileName;
 
         std::optional<teddy::pla_file> pla_file = teddy::pla_file::load_file(pla_path);
-        if (!pla_file.has_value()) { 
-            continue; 
+        if (!pla_file.has_value()) {
+            continue;
         }
 
         teddy::pla_file* pla = &pla_file.value();
@@ -99,96 +145,84 @@ int main() {
         teddy::bss_manager manager(number_of_vars, 33'000);
 
         // Disable automatic variable reordering.
-        manager.set_auto_reorder(false); 
+        manager.set_auto_reorder(false);
 
         std::cout << "There is " << std::to_string(number_of_functions) << " functions with " << std::to_string(number_of_vars) << " variables in file: " + std::string(find_file_data.cFileName) << std::endl;
         if (comparing_option == "ORIGINAL") {
             for (int i = 0; i < number_of_functions; ++i) {
                 // get function from pla file
                 teddy::bss_manager::diagram_t diagram = manager.from_pla(*pla, teddy::fold_type::Tree)[i];
-                
+
                 // save statistics
                 int node_count = manager.get_node_count(diagram);
                 //std::cout << "Number of nodes in diagram (including terminal nodes): " << node_count << std::endl;
                 sum_of_node_counts += node_count;
                 number_of_diagrams += 1;
             }
-        } else if (comparing_option == "DESCENDING_TD") {
-            for (int i = 0; i < number_of_functions; ++i) {
-                // get function from pla file
-                teddy::bss_manager::diagram_t diagram = manager.from_pla(*pla, teddy::fold_type::Tree)[i];
+        }
+        else if (comparing_option == "DESCENDING_TD") {
+            use_derivatives(sum_of_node_counts, number_of_diagrams, number_of_functions, number_of_vars, manager, pla, false);
+        }
+        else if (comparing_option == "ASCENDING_TD") {
+            use_derivatives(sum_of_node_counts, number_of_diagrams, number_of_functions, number_of_vars, manager, pla, true);
+        }
+        else if (comparing_option == "RANDOM") {
+            int number_of_replications = 100;
+            for (int r = 0; r < number_of_replications; ++r) {
+                for (int i = 0; i < number_of_functions; ++i) {
+                    // get function from pla file
+                    teddy::bss_manager::diagram_t diagram = manager.from_pla(*pla, teddy::fold_type::Tree)[i];
 
-                // list for true densities of all variables of this function
-                std::vector<var> list_for_reordering = std::vector<var>(number_of_vars);
-                
-                // save all true densities of each variables in this function in list_for_reordering
-                get_td_of_all_vars_in_function(manager, list_for_reordering, diagram);
-                
-                // sort list of structs based on true density
-                std::sort(list_for_reordering.begin(), list_for_reordering.end(), compare_by_true_density_desc);
-                
-                // vector with new order in teddy format 
-                std::vector<teddy::int32> order_after = std::vector<teddy::int32>(number_of_vars);
+                    // list of indexes of all variables of this function
+                    std::vector<int> list_for_random_order = std::vector<int>(number_of_vars);
 
-                // moving reordered list to teddy format vector
-                for (int i = 0; i < number_of_vars; ++i) {
-                    order_after[i] = list_for_reordering[i].variable;
+                    // populate list with indexes  
+                    //  indexes of vector -> [0][1][2][3]
+                    //          variables -> |0||1||2||3|    
+                    // means that variable x0 is first in order, x1 is second and so on ...                   
+                    for (int j = 0; j < number_of_vars; j++) {
+                        list_for_random_order[j] = j;
+                    }
+
+                    // Create a random engine
+                    std::random_device rd;  // Obtain a random number from hardware
+                    std::mt19937 eng(rd()); // Seed the generator
+
+                    // Shuffle the vector
+                    //  indexes of vector -> [0][1][2][3]
+                    //          variables -> |1||3||0||2|    
+                    // means that variable x0 is third in order, x1 is first and so on ...  
+                    std::shuffle(list_for_random_order.begin(), list_for_random_order.end(), eng);
+
+                    // vector with new order in teddy format 
+                    std::vector<teddy::int32> order_after = std::vector<teddy::int32>(number_of_vars);
+
+                    // moving randomly ordered list to teddy format vector
+                    for (int i = 0; i < number_of_vars; ++i) {
+                        order_after[i] = list_for_random_order[i];
+                    }
+
+                    // creating manager with new order of variables based on true density
+                    teddy::bss_manager manager_after(number_of_vars, 1'000, order_after);
+                    manager_after.set_auto_reorder(false);
+                    teddy::bss_manager::diagram_t diagram_after = manager_after.from_pla(*pla, teddy::fold_type::Tree)[0];
+
+                    // save statistics
+                    int node_count = manager_after.get_node_count(diagram_after);
+                    sum_of_node_counts += node_count;
+                    number_of_diagrams += 1;
                 }
-
-                // creating manager with new order of variables based on true density
-                teddy::bss_manager manager_after(number_of_vars, 1'000, order_after);
-                manager_after.set_auto_reorder(false);
-                teddy::bss_manager::diagram_t diagram_after = manager_after.from_pla(*pla, teddy::fold_type::Tree)[0];
-
-                // save statistics
-                int node_count = manager_after.get_node_count(diagram_after);
-                sum_of_node_counts += node_count;
-                number_of_diagrams += 1;
             }
-        } else if (comparing_option == "ASCENDING_TD") {
-            for (int i = 0; i < number_of_functions; ++i) {
-                // get function from pla file
-                teddy::bss_manager::diagram_t diagram = manager.from_pla(*pla, teddy::fold_type::Tree)[i];
-
-                // list for true densities of all variables of this function
-                std::vector<var> list_for_reordering = std::vector<var>(number_of_vars);
-
-                // save all true densities of each variables in this function in list_for_reordering
-                get_td_of_all_vars_in_function(manager, list_for_reordering, diagram);
-
-                // sort list of structs based on true density
-                std::sort(list_for_reordering.begin(), list_for_reordering.end(), compare_by_true_density_asc);
-
-                // vector with new order in teddy format 
-                std::vector<teddy::int32> order_after = std::vector<teddy::int32>(number_of_vars);
-
-                // moving reordered list to teddy format vector
-                for (int i = 0; i < number_of_vars; ++i) {
-                    order_after[i] = list_for_reordering[i].variable;
-                }
-
-                // creating manager with new order of variables based on true density
-                teddy::bss_manager manager_after(number_of_vars, 1'000, order_after);
-                manager_after.set_auto_reorder(false);
-                teddy::bss_manager::diagram_t diagram_after = manager_after.from_pla(*pla, teddy::fold_type::Tree)[0];
-
-                // save statistics
-                int node_count = manager_after.get_node_count(diagram_after);
-                sum_of_node_counts += node_count;
-                number_of_diagrams += 1;
-                manager_after.clear_cache();
-            } 
-        } else if (comparing_option == "RANDOM") {
-            // pass
+            sum_of_node_counts /= number_of_replications;
+            number_of_diagrams /= number_of_replications;
         }
     } while (FindNextFile(h_find, &find_file_data) != 0);
+    
     FindClose(h_find);
 
-    // print statistics
-    if (number_of_diagrams != 0) {
-        double average_number_of_nodes = sum_of_node_counts / number_of_diagrams;
-        std::cout << "Average number of nodes with " + comparing_option + " order is: " + std::to_string(average_number_of_nodes) << std::endl;
-    }  
+    double average_number_of_nodes = sum_of_node_counts / number_of_diagrams;
+    std::cout << "Average number of nodes with " + comparing_option + " order is: " + std::to_string(average_number_of_nodes) << std::endl;
+     
 }
 
     
